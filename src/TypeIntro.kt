13b897b7443bfc6136bbb7a4452de30c58ed1ff6
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-25 17:11:15 -0500

    Update

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,8 +1,22 @@
-// Removed args: Array<String> from the function main parameters.
-// No longer necessary
+const val MAX_EXPERIENCE = 5000
+
 fun main() {
-    // removed :Int, no longer necessary
-    // removed semicolons
+    var playerName = "Estragon"
     var experiencePoints = 5
+
+    // Challenge: hasSteed
+    var hasSteed = false
+
+    // Challenge: The Unicorn's Horn
+    var pubName = "The Unicorn's Horn"
+    var publicanName = "Publican"
+    var currentGold = 50
+    var drinkMenu: List<String> = listOf("Mead", "Wine", "LaCroix")
+
+    experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
+
+    // Challenge: Magic Mirror
+    println(playerName.reversed())
 }
